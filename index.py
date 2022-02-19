a74788e5e9040efde982be626246c4693a500381
anasstasiam <52708233+anasstasiam@users.noreply.github.com>
2022-02-19 22:06:56 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,6 @@
+import random
+num = input('login ')
+pas = ''
+for x in range(16):
+    pas = pas + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+print('Hello, ', num, 'your password is: ', pas)
